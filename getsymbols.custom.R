15a98a30c89d892aaade14e96df66ff27e016c5d
Tom <tom.olsson.1@gmail.com>
2014-09-18 11:58:57 +0200

    Added getSymbols.CSV2

--- getsymbols.custom.R
+++ getsymbols.custom.R
@@ -39,6 +39,95 @@ function (fr, return.class)
     }
 }
 
+getSymbols.CSV2 <- 
+function (
+Symbols, env, dir = "", return.class = "xts", 
+extension = "csv", 
+cols = list(
+  Date = 1,
+  Open = 2,
+  High = 3,
+  Low = 4,   
+  Close = 5,
+  Volume = 6,
+  Adjusted = 7      
+),
+verbose = F,
+auto.assign = T,
+...)
+{
+importDefaults("getSymbols.csv")
+this.env <- environment()
+for (var in names(list(...))) {
+  assign(var, list(...)[[var]], this.env)
+}
+
+default.tz = Sys.timezone()
+default.return.class <- return.class
+default.dir <- dir
+default.extension <- extension
+default.cols <- cols
+if (missing(verbose))
+  verbose <- FALSE
+
+if (missing(auto.assign))
+  auto.assign <- TRUE
+
+for (i in 1:length(Symbols)) {
+  return.class <- getSymbolLookup()[[Symbols[[i]]]]$return.class
+  return.class <- ifelse(is.null(return.class), default.return.class,return.class)
+  dir <- getSymbolLookup()[[Symbols[[i]]]]$dir
+  dir <- ifelse(is.null(dir), default.dir, dir)
+  extension <- getSymbolLookup()[[Symbols[[i]]]]$extension
+  extension <- ifelse(is.null(extension), default.extension,extension)
+  tz <- getSymbolLookup()[[Symbols[[i]]]]$tz
+  tz <- ifelse(is.null(tz), default.tz, tz)
+  
+  cols <- getSymbolLookup()[[Symbols[[i]]]]$cols
+  cols <- ifelse(is.null(cols), default.cols, cols)
+  
+  if (verbose)
+	cat("loading ", Symbols[[i]], ".....")
+  
+  if (dir == "") {
+	sym.file <- paste(Symbols[[i]], extension, sep = ".")
+  } else {
+	sym.file <- file.path(dir, paste(Symbols[[i]], extension,sep = "."))
+  }
+  
+  if (!file.exists(sym.file)) {
+	cat("\nfile ",paste(Symbols[[i]],"csv",sep=".")," does not exist ","in ",dir,"....skipping\n")
+	next
+  }
+  fr = read.csv(sym.file,header=T,stringsAsFactors=F ) #added header=TRUE      
+
+  if(is.null(cols$Date))
+	cols$Date = grep(pattern="date|index",x=colnames(fr),ignore.case=T)  
+
+  cols = lapply(cols,function(x,y) if(is.numeric(x)) x else match(x,y),y=colnames(fr))      
+  cols[cols > ncol(fr)] = NA
+  
+  dates = as.Date(fr[,cols$Date],tz=tz)  
+  cols$Date = NULL
+  
+  if (verbose)
+	cat("done.\n")
+
+  fr = do.call("cbind",sapply(cols,FUN=function(x,fr) if(is.na(x)) NA else as.numeric(fr[,x]),fr=fr))
+  fr = xts(fr,dates,src = "csv", updated = Sys.time())              
+  
+  fr <- convert.time.series(fr = fr, return.class = return.class)
+  Symbols[[i]] <- toupper(gsub("\\^", "", Symbols[[i]]))
+  
+  if (auto.assign)
+	assign(Symbols[[i]], fr, env)
+}
+if (auto.assign)
+  return(Symbols)
+
+return(fr)
+}      
+  
 getSymbols.John <- 
 function (
 	Symbols, 
