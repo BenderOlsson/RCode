7c56290736e543dbeff46f66e8cac21de8e47f61
Tom <tom.olsson.1@gmail.com>
2014-10-08 09:16:02 +0200

    Adderat Mcap + has.Mcap

--- utils.custom.R
+++ utils.custom.R
@@ -12,3 +12,21 @@ matrix.to.df <- function(x, header = NA,...){
   }  
   as.data.frame(x,...)  
 }
+
+has.Mcap <- function (x, which = FALSE) 
+{
+  colAttr <- attr(x, "Mcap")
+  if (!is.null(colAttr)) 
+    return(if (which) colAttr else TRUE)
+  loc <- grep("Mcap", colnames(x), ignore.case = TRUE)
+  if (!identical(loc, integer(0))) {
+    return(if (which) loc else TRUE)
+  }
+  else FALSE
+}
+
+Mcap <- function (x){
+  if (has.Mcap(x)) 
+    return(x[, grep("Mcap", colnames(x), ignore.case = TRUE)])
+  stop("subscript out of bounds: no column name containing \"Mcap\"")
+}
